3f8f839630b9fcb84571a80a46b20c517fd8b9b1
Juliana <juliana6k@mail.ru>
2024-03-05 14:00:22 +0300

    один продукт стр

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from catalog.views import home
 from catalog.views import contacts
+from catalog.views import product
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -8,4 +9,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("", home, name="home"),
     path("contacts/", contacts, name="contacts"),
+    path("product/<int:pk>", product, name="product"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from catalog.models import Product
 
 def home(request):
     return render(request, "catalog/home.html")
@@ -13,3 +13,9 @@ def contacts(request):
         print(f'{name} ({phone}) {message}')
 
     return render(request, "catalog/contacts.html")
+
+
+def product(request, pk):
+    context = {'object': Product.objects.get(pk=pk)}
+    return render(request, "catalog/product.html", context)
+
