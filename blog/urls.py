1eb8dbfeaf4242db6e0bc6f1c010e3f483049fd0
Juliana <juliana6k@mail.ru>
2024-03-15 16:28:23 +0300

    Crud

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from blog.apps import BlogConfig
-from blog.views import PostCreateView, PostListView, PostDetailView
+from blog.views import PostCreateView, PostListView, PostDetailView, PostUpdateView, PostDeleteView
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -11,4 +11,7 @@ urlpatterns = [
     path("create/", PostCreateView.as_view(), name="create"),
     path("post_list/", PostListView.as_view(), name="post_list"),
     path("detail/<int:pk>/", PostDetailView.as_view(), name="details"),
+    path("update/<int:pk>/", PostUpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", PostDeleteView.as_view(), name="delete"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from blog.models import Post
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 
@@ -13,6 +13,18 @@ class PostCreateView(CreateView):
 class PostListView(ListView):
     model = Post
 
+
+class PostDeleteView(DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:post_list')
+
+
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ('title', 'body', 'preview')
+    success_url = reverse_lazy("blog:post_list")
 
