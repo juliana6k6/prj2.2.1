21b3271f954778271d59866b2678c175e85895b6
Juliana <juliana6k@mail.ru>
2024-03-15 17:15:46 +0300

    get_object переопредилили

--- blog/views.py
+++ blog/views.py
@@ -22,6 +22,13 @@ class PostDeleteView(DeleteView):
 class PostDetailView(DetailView):
     model = Post
 
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.view_count += 1
+        self.object.save()
+        return self.object
+
+
 
 class PostUpdateView(UpdateView):
     model = Post
