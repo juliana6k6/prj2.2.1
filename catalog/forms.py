3f20c18cef0e4ce6ce544f44c1a045674d59bd68
Juliana <juliana6k@mail.ru>
2024-03-22 15:25:49 +0300

    clean_data

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, forms
 
 from catalog.models import Product
 
@@ -7,3 +7,21 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data.get('name').lower()
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        for word in forbidden_words:
+            if word in cleaned_data:
+                raise forms.ValidationError(f'В названии продукта не должно быть слова {word}')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data.get('description').lower()
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+        for word in forbidden_words:
+            if word in cleaned_data:
+                raise forms.ValidationError(f'В описании продукта не должно быть слова {word}')
+        return cleaned_data
+
+
