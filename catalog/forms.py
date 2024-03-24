dc94aab44740c8f37a611e6129e6703cd6371aa4
Juliana <juliana6k@mail.ru>
2024-03-24 20:52:59 +0300

    стиль форм

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,20 @@
-from django.forms import ModelForm, forms
+from django.forms import ModelForm, forms, BooleanField
 
 from catalog.models import Product, Version
 
 
-class ProductForm(ModelForm):
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+
+class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
@@ -26,7 +37,7 @@ class ProductForm(ModelForm):
         return cleaned_data
 
 
-class VersionForm(ModelForm):
+class VersionForm(StyleFormMixin, ModelForm):
 
     class Meta:
         model = Version
